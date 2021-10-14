38804e27e54e96a5448d4fe16e156f3f947c9b97
AlexHamn <agalexandro16@gmail.com>
2021-10-13 19:44:52 -0500

    make popUp apear and disapear

--- src/index.js
+++ src/index.js
@@ -1,18 +1,69 @@
 import './style.css';
 
-function appendItem(item, iiif, id) {
-  const list = document.getElementById('works');
-  const li = document.createElement('li');
-  li.innerHTML = `<div>
-    <img src="${iiif}/${id}/full/843,/0/default.jpg" alt="${item.data.title}">
-    <h4>${item.data.title}</h4>
-    <p>by ${item.data.artist_title}</p><p class="medium">${item.data.medium_display}</p>
-    <button>Comments</button>
+function modal(item, iiif, imageId, id) {
+  const { body } = document;
+  const popUp = document.createElement('div');
+  popUp.id = 'overlay';
+  body.append(popUp);
+  popUp.innerHTML = `<div id=${id} class="modal">
+        <img src="#" alt="artwork">
+        <button id="close" onclick ="this.remove(), overlay.remove()">&times;</button>
+        <h3>Artwork</h3>
+        <div id="info">
+            <p>by Artist</p>
+            <p>medium</p>
+        </div>
+        <div id="comments">
+            <h4>Comments (num)</h4>
+            <ul>
+                <li>
+                    <p>Comment</p>
+                </li>
+                <li>
+                    <p>Comment</p>
+                </li>
+                <li>
+                    <p>Comment</p>
+                </li>
+            </ul>
+        </div>
+        <div id="addComment">
+            <h4>Add a comment</h4>
+            <form action="post">
+                <input type="text" id="user" name="user" placeholder="Your name">
+                <input type="text" id="comment" name="comment" placeholder="Your insights">
+                <button type="submit" id="addButton">comment</button>
+            </form>
+        </div>
     </div>`;
-  list.append(li);
 }
 
-async function displayCat(apiLink) {
+function appendItem(item, iiif, imageId, id) {
+  const list = document.getElementById('works');
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const h4 = document.createElement('h4');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const button = document.createElement('button');
+  li.id = id;
+  li.append(div);
+  div.innerHTML = `
+    <img src="${iiif}/${imageId}/full/843,/0/default.jpg" alt="${item.data.title}">
+    <h4>${item.data.title}</h4>
+    <p>by ${item.data.artist_title}</p><p class="medium">${item.data.medium_display}</p>`;
+  div.append(button);
+  img.src = `${iiif}/${imageId}/full/843,/0/default.jpg`;
+  img.alt = `${item.data.title}`;
+  button.id = id;
+  button.class = 'commentButton';
+  button.textContent = 'Comments';
+  list.append(li);
+  button.addEventListener('click', () => { modal(id); });
+}
+
+async function displayItem(apiLink) {
   const response = await fetch(`${apiLink}?fields=id,title,image_id,artist_title,medium_display`, {
     method: 'GET',
     headers: {
@@ -26,14 +77,15 @@ async function displayCat(apiLink) {
   const result = await data;
 
   const iiif = await data.config.iiif_url;
-  const id = await data.data.image_id;
+  const imageId = await data.data.image_id;
+  const id = await data.data.id;
 
-  appendItem(result, iiif, id);
+  appendItem(result, iiif, imageId, id);
 }
 
-async function callCats() {
+async function call() {
   const cats = 'https://api.artic.edu/api/v1/artworks/search?q=modern&size=12';
-  let catBox = [];
+  let itemsArray = [];
 
   const response = await fetch(cats, {
     method: 'GET',
@@ -47,17 +99,11 @@ async function callCats() {
   // console.log(data)
   const result = await data.data;
 
-  catBox = result.map((e) => e.api_link);
+  itemsArray = result.map((e) => e.api_link);
 
-  catBox.forEach((e) => {
-    displayCat(e);
+  itemsArray.forEach((e) => {
+    displayItem(e);
   });
 }
 
-callCats();
-
-const modal = document.getElementById('modal');
-const overlay = document.getElementById('overlay');
-const closeButton = document.getElementById('close');
-closeButton.setAttribute('onclick', 'modal.remove(), overlay.remove()');
-// closeButton.setAttribute('onclick', 'overlay.remove()');
+call();
